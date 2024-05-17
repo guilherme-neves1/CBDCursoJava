91124a7c188d686f1458ff654ef9bbb6b4885fde
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-17 16:37:42 -0300

    aula3

---
+++ Aula3.java
@@ -0,0 +1,58 @@
+public class Aula3 {
+  // COMANDOS DE DECISÃO
+  public static void main(String[] args) {
+
+    int nota = 85;
+    int media = 60;
+    int faltas = 10;
+    int maxFaltas = 5;
+
+    // Ternário
+    String res;
+    int res2;
+
+    res = (nota >= media ? "Aprovado\n" : "Reprovado\n");
+    System.out.println("\nResultado: " + res);
+
+    res2 = (nota >= media ? 1 : 0);
+    System.out.println("\nResultado: " + (res2 == 1 ? "Aprovado\n" : "Reprovado\n"));
+
+    if(nota >= media && faltas <= maxFaltas) {
+      System.out.println("Aprovado");
+    } else if(nota >= 40) {
+      System.out.println("Recuperação");
+    } else {
+      System.out.println("Reprovado");
+    }
+
+    System.out.println("Fim do programa\n");
+
+    int pos = 4;
+
+    switch(pos) {
+      case 1:
+        System.out.println("Primeiro lugar!");
+        break;
+      case 2:
+        System.out.println("Segundo lugar!");
+        break;
+      case 3:
+        System.out.println("Terceiro lugar!");
+        break;
+      default:
+        System.out.println("Não subiu no pódio!");
+        break;
+
+
+    }
+
+
+
+  }
+  
+
+
+
+
+
+}
